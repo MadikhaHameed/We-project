70bd19489644de269ce0dc344477f709e2636ed6
MadikhaHameed <88433641+MadikhaHameed@users.noreply.github.com>
2021-08-04 04:46:25 -0700

    Add files via upload

---
+++ chunks/footer.php
@@ -0,0 +1,28 @@
+<section class="ffooter">
+		<footer class="page-footer">
+          <div class="container">
+            <div class="row">
+              <div class="col l6 s12">
+                <h5 class="white-text">Contact US</h5>
+                <p class="grey-text text-lighten-4">123, Madikkha Hameed, Karachi, Pakistan.</p>
+                <p class="grey-text text-lighten-4">Phone : 021673745752</p>
+              </div>
+              <div class="col l4 offset-l2 s12">
+                <h5 class="white-text">Social Media Links</h5>
+                <ul>
+                  <li><a class="grey-text text-lighten-3" href="#!">Facebook</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Instagram</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Twitter</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Whatsapp</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="footer-copyright">
+            <div class="container">
+            © 2021 Copyright @ Fastfoods
+            <a class="grey-text text-lighten-4 right" href="#!">Made in pakistan with <span><i class="tiny material-icons">favorite</i></span></a>
+            </div>
+          </div>
+        </footer>
+	</section>
