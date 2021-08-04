c472363c0476c7898270b1b7129ce7675f6fdc78
madikha <madikhahameed@gmail.com>
2021-08-04 08:59:52 -0700

    added remote mysql connection

--- backends/config.php
+++ backends/config.php
@@ -1,8 +1,8 @@
 <?php
 
-$host = "sql104.epizy.com";
-$user= "epiz_29322690";
-$pwd = "MxqbK3EuZpEyAHF";
-$database = "epiz_29322690_madhary";
+$host = "remotemysql.com";
+$user= "PTFXfxWWTs";
+$pwd = "FWYXJclsZM";
+$database = "PTFXfxWWTs";
 
 ?>
